9e98bd7db58af47e4a7adcc92db43c552c754082
bibek Timalsina <120554874+Timalcnabibek@users.noreply.github.com>
2023-02-08 18:56:09 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,2 @@
+alert("you have come to bibek timalsina's website!");
+console.log("i am bibek timalsina");
---
+++ reference.js
@@ -0,0 +1,2 @@
+console.log("this is the reference part");
+alert("this is the reference page of the whole website!")
